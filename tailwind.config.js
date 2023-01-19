9103240a46fc16f97ee36fe1fca7bb71b4173bc1
amir <amirazizihosein@gmail.com>
2023-01-20 01:08:59 +0330

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      lx: "1440px",
+    },
+    extend: {
+      colors: {
+        darkcyan: "hsl(185, 75%, 39%)",
+        verydarkdesaturatedblue: "hsl(229, 23%, 23%)",
+        darkgrayishblue: "hsl(227, 10%, 46%)",
+        darkgray: "hsl(0, 0%, 59%)",
+      },
+    },
   },
   plugins: [],
 };
